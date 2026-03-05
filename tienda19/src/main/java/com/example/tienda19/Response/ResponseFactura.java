4b44076fb1ee6ec932f59b4a4a49656562c04033
LenriqueIslas <luisenrique1997cistic@gmail.com>
2026-03-05 01:34:29 -0600

    Se cambia la version de spridoc 2.7.0 se realiza swagger

--- tienda19/src/main/java/com/example/tienda19/Response/ResponseFactura.java
+++ tienda19/src/main/java/com/example/tienda19/Response/ResponseFactura.java
@@ -1,8 +1,22 @@
-package com.example.tienda19.Response;
+package com.example.tienda19.response;
+
+import lombok.*;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Transactional
+@Builder
+
 
 public class ResponseFactura {
-
-    private String razonsociaL;
+    private long id;
+    private double montoFactura;
+    private Integer codigoPostal;
+    private String razonsocial;
     private String regimensocial;
-    private int montoFactura;
 }
+
